b4659951f9d0975cef6ac7c392a4d7c14f1bcaa3
xuxa <xuexu6666@gmail.com>
2019-09-28 17:52:36 -0500

    Setting Bitmap Pixels

--- Fractal_Bitmap/Fractal_Bitmap/Main.cpp
+++ Fractal_Bitmap/Fractal_Bitmap/Main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include "Bitmap.h"
 
-using namespace std;
 using namespace caveofprogramming;
 
 int main() {
 
-	Bitmap bitmap(800, 600);
+	int const WIDTH = 800;
+	int const HEIGHT = 600;
+
+	Bitmap bitmap(WIDTH, HEIGHT);
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			bitmap.setPixel(x, y, 255, 0, 0);
+		}
+	}
 
 	bitmap.write("test.bmp");
 
-	cout << "Finished." << endl;
+	std::cout << "Finished." << std::endl;
 	return 0;
 }

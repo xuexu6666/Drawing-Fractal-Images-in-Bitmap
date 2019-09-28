040c9b3158f7e5a2b7f389269ad0c31229e6c84e
xuxa <xuexu6666@gmail.com>
2019-09-28 18:13:57 -0500

    Fractal Algoritm and Image

--- Fractal_Bitmap/Fractal_Bitmap/Main.cpp
+++ Fractal_Bitmap/Fractal_Bitmap/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Bitmap.h"
+#include "Mandelbrot.h"
 
 using namespace caveofprogramming;
 
@@ -10,9 +11,23 @@ int main() {
 
 	Bitmap bitmap(WIDTH, HEIGHT);
 
+	double min = 999999;
+	double max = -999999;
+
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			bitmap.setPixel(x, y, 255, 0, 0);
+			double xFractal = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
+			double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
+
+			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+
+			uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+
+			color = color * color * color;
+
+			bitmap.setPixel(x, y, 0, color, 0);
+			if (color < min) min = color;
+			if (color > max) max = color;
 		}
 	}
 
---
+++ Fractal_Bitmap/Fractal_Bitmap/src/Mandelbrot.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace caveofprogramming {
+
+	class Mandelbrot {
+	public:
+		static const int MAX_ITERATIONS = 1000;
+
+	public:
+		Mandelbrot();
+		virtual ~Mandelbrot();
+
+		static int getIterations(double x, double y);
+	};
+
+}

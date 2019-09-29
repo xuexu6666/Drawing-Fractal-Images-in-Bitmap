6a8f2f683d9f5869861fbe0792c1ac8042c19285
xuxa <xuexu6666@gmail.com>
2019-09-28 19:10:57 -0500

    Using Color Histogram and Transforms

--- Fractal_Bitmap/Fractal_Bitmap/Main.cpp
+++ Fractal_Bitmap/Fractal_Bitmap/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 
@@ -14,6 +15,9 @@ int main() {
 	double min = 999999;
 	double max = -999999;
 
+	std::vector<int> histogram(Mandelbrot::MAX_ITERATIONS, 0);
+	std::vector<int> fractal(WIDTH * HEIGHT, 0);
+
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
 			double xFractal = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
@@ -21,13 +25,40 @@ int main() {
 
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-			uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+			fractal[y * WIDTH + x] = iterations;
 
-			color = color * color * color;
+			if (iterations != Mandelbrot::MAX_ITERATIONS) {
+				histogram[iterations]++;
+			}
+		}
+	}
 
-			bitmap.setPixel(x, y, 0, color, 0);
-			if (color < min) min = color;
-			if (color > max) max = color;
+	int total = 0;
+	for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++) {
+		total += histogram[i];
+	}
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+
+			uint8_t red = 0;
+			uint8_t green = 0;
+			uint8_t blue = 0;
+
+			int iterations = fractal[y * WIDTH + x];
+
+			if (iterations != Mandelbrot::MAX_ITERATIONS) {
+
+				double hue = 0.0;
+
+				for (int i = 0; i <= iterations; i++) {
+					hue += ((double)histogram[i]) / total;
+				}
+
+				green = pow(255, hue);
+			}
+
+			bitmap.setPixel(x, y, red, green, blue);
 		}
 	}
 
